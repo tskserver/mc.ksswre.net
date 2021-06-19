98871d4113cf8d550b4378b67e636087a2346f3f
k <ksswre@yahoo.co.jp>
2021-06-20 00:25:13 +0900

    use /api/status instead

--- scripts/script.js
+++ scripts/script.js
@@ -1,67 +1,66 @@
-
-function fetch_status(server, map) {
-	var xhr = new XMLHttpRequest();
+function fetch_status(callback) {
+	let xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function() {
+		let obj = null;
 		if (this.readyState == 4 && this.status == 200) {
-			var obj = JSON.parse( this.responseText );
-			
-			Object.keys(obj).forEach(function (key) {
-				
-				if ( !(key in map) ) {
-					map[key] = key;
-				}
-				
-				var e = document.getElementById(server + "-" + map[key]);
-				if (e) {
-					while (e.firstChild) {
-						e.removeChild(e.firstChild);
-					}
-					
-					var value = obj[key];
-					if (obj[key] == true) {
-						value = "Yes";
-					}
-					if (obj[key] == false){
-						value = "No";
-					}
-					
-					if (key == "last_checked_at") {
-						var d = parseInt((new Date()).getTime() / 1000) - obj[key];
-						value = "(last checked: " + d + "s ago)";
-					}
-					
-					var t = document.createTextNode(value);
-					
-					if (key == "is_running") {
-						var icon = document.createElement("i");
-						if (obj[key]) {
-							icon.className = "fa fa-check";
-							e.className += " green bold";
-						} else {
-							icon.className = "fa fa-close";
-							e.className += " red bold";
-						}
-						e.className += " a-rotation a-ease-in";
-						e.appendChild(icon);
-					}
-					
-					e.appendChild(t);
-				}
-			});
+			obj = JSON.parse( this.responseText );
+		}
+		if (callback) {
+			callback(obj);
 		}
 	}
-
-	xhr.open("GET", "/check/status/" + server + ".php?" + Math.random());
+	xhr.open("GET", "/api/status");
 	xhr.send(null);
 }
 
+function clear_child_element(e){
+	// clear inner element: only <i> (loading icon) is listed
+	while (e.firstChild) {
+		e.removeChild(e.firstChild);
+	}
+}
+
+function redraw_text(servername, key, value){
+	let e = document.getElementById(servername + "-" + key);
+	clear_child_element(e);
+	let t = document.createTextNode(value);
+	e.appendChild(t);
+}
+
+function redraw_is_running(servername, key, value){
+	let is_online = (value == 'online') ? true : false;
+	let e = document.getElementById(servername + "-" + key);
+	clear_child_element(e);
+	let elm = document.createElement("span");
+	let icon = document.createElement("i");
+	let text;
+	if (is_online) {
+		icon.className = "fa fa-check";
+		elm.className += " green bold";
+		text = 'Yes';
+	} else {
+		icon.className = "fa fa-close";
+		elm.className += " red bold";
+		text = 'No';
+	}
+	elm.className += " a-rotation a-ease-in";
+	elm.appendChild(icon);
+	elm.appendChild(document.createTextNode((is_online) ? 'Yes' : 'No'));
+	e.appendChild(elm);
+	return elm;
+}
+
 window.onload = function() {
-	fetch_status("tskserver", {
-		is_running: "is-running",
-		last_checked_at: "last-checked-at",
-	});
-	fetch_status("skyblock", {
-		is_running: "is-running",
-		last_checked_at: "last-checked-at",
-	});
+	fetch_status(function(response){
+		if (response == null) { return; }
+		Object.keys(response).forEach(function (servername) {
+			let info = response[servername];
+			redraw_is_running(servername, 'is-running', info['status']);
+			redraw_text(servername, 'version', info['body']['version']['name']);
+			let str_status = "" + info['body']['players']['online'] + "/" + info['body']['players']['max'];
+			redraw_text(servername, 'status', str_status);
+			let str_last_checked_at = '(checked at: ' + info['last_checked_at'] + ')';
+			redraw_text(servername, 'last-checked-at', str_last_checked_at);
+		});
+	})
 }
